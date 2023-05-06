7975729aad0a30e2e2f76d674d40cdd432c0d87a
L <25758530+Lece619@users.noreply.github.com>
2023-05-06 14:05:37 +0900

    feat: smell 19 ~ Insider_trading

--- src/main/java/me/whiteship/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/PremiumBooking.java
+++ src/main/java/me/whiteship/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/PremiumBooking.java
@@ -2,6 +2,7 @@ package me.whiteship.refactoring._18_middle_man._40_replace_subclass_with_delega
 
 import java.time.LocalDateTime;
 
+//Is not used
 public class PremiumBooking extends Booking {
 
     private PremiumExtra extra;
@@ -11,17 +12,4 @@ public class PremiumBooking extends Booking {
         this.extra = extra;
     }
 
-    @Override
-    public boolean hasTalkback() {
-        return this.show.hasOwnProperty("talkback");
-    }
-
-    @Override
-    public double basePrice() {
-        return Math.round(super.basePrice() + this.extra.getPremiumFee());
-    }
-
-    public boolean hasDinner() {
-        return this.extra.hasOwnProperty("dinner") && !this.isPeakDay();
-    }
 }
